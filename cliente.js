b1ffa01a20f3fc4fb18475d2040cafbb64cc3e27
jhoyabiko <jhoyabiko8@gmail.com>
2025-07-24 09:44:31 -0300

    Update cliente.js

--- cliente.js
+++ cliente.js
@@ -163,3 +163,9 @@ if (idCliente) {
   carregarCliente();
   carregarEmprestimos();
 }
+const linkParcelado = document.createElement("a");
+linkParcelado.href = `parcelado.html?id=${idCliente}`;
+linkParcelado.textContent = "Parcelado";
+linkParcelado.classList.add("botao-navegacao");
+document.querySelector(".botoes-navegacao").appendChild(linkParcelado);
+
